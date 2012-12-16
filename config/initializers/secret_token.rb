331a6434741d9328b0c08cab4e502ef0678b5f52
Asaf Schers <asafschers@Asaf-Scherss-MacBook.local>
2012-12-16 19:55:26 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniRoomer::Application.config.secret_token = '06ef19deb5b067a39180fb3e7cb7778122e85bdc605d66eb56419cd239d7b08e20c3d34f65beccb8bc40b27779db636c1424ca20d9538dd715da97650d032cd7'
