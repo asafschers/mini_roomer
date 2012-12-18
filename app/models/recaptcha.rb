c8caccadf061b32183705332a2f36a0c12465658
Asaf Schers <asafschers@Asaf-Scherss-MacBook.local>
2012-12-18 12:19:36 +0200

    captcha

--- app/models/recaptcha.rb
+++ app/models/recaptcha.rb
@@ -3,7 +3,7 @@ class Recaptcha
   def self.verify(args)
     url = 'http://www.google.com/recaptcha/api/verify'
     query_parts = []
-    query_parts << "privatekey=#{6LdbgdoSAAAAAL5JbVbLpqYSeoRUEVT9uLNXbkBE}"
+    query_parts << "privatekey=#{'6LdbgdoSAAAAAL5JbVbLpqYSeoRUEVT9uLNXbkBE'}"
     query_parts << "remoteip=#{args[:ip_address]}"
     query_parts << "challenge=#{args[:recaptcha_challenge_field]}"
     query_parts << "response=#{args[:recaptcha_response_field]}".gsub(' ', '+')
